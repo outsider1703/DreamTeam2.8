ec2a1c510c108e4ac513edc6ca1824ec807e1bc9
Aleksandr Kovalov <etos.klub@gmail.com>
2020-06-13 01:00:33 +0300

    1

---
+++ DreamTeam2.8/DataManagers/IngredientsDataManager.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  DreamTeam2.8
+//
+//  Created by Apple on 13.06.2020.
+//  Copyright © 2020 Aleksandr Kovalov. All rights reserved.
+//
+
+class IngredientsDataManager {
+    
+    let meat = ["Рыба", "Свинина", "Говядина", "Курица"]
+    
+    let vegetables = ["Морковь", "Лук", "Чеснок",
+                      "Томат", "Капуста", "Свекла", "Картофель"]
+    
+    let garnish = ["Макароны", "Гречка", "Рис", "Картофель"]
+    
+}
---
+++ DreamTeam2.8/Models/IngredientsForRecipes.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  DreamTeam2.8
+//
+//  Created by Apple on 13.06.2020.
+//  Copyright © 2020 Aleksandr Kovalov. All rights reserved.
+//
+
+struct IngredientsForRecipes {
+    
+    let meet: String?
+    let vegetables: [String]?
+    let garnish: String?
+    
+}
