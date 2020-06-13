fac82bef2d518b98eb637fdc4d8505633b94d6ab
Igor P-V <ispogiba@gmail.com>
2020-06-13 03:52:32 +0300

    second commit

---
+++ DreamTeam2.8/ViewControllers/ConstructorViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ConstructorViewController.swift
+//  DreamTeam2.8
+//
+//  Created by Elena Igumenova on 13.06.2020.
+//  Copyright © 2020 Aleksandr Kovalov. All rights reserved.
+//
+
+import UIKit
+
+class ConstructorViewController: UIViewController {
+
+    @IBOutlet var foodButton: [UIButton]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
