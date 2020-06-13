13d4ef87c11bd174d59f5b9c570ca08dca6f4832
outsider1703 <simka-simka020697simka-simka1>
2020-06-14 00:37:11 +0200

    update Information in Menu Model

--- DreamTeam2.8/Models/GeneralMenuForTheApplication.swift
+++ DreamTeam2.8/Models/GeneralMenuForTheApplication.swift
@@ -12,43 +12,61 @@ struct GeneralMenuForTheApplication {
     let mainPosition: MainPosition
     let ingredients: [String]?
     var garnish: String? = nil
+    let information: String
+    
     
 }
 
 //MARK: - Privat Function For Menu
 extension GeneralMenuForTheApplication {
     
-   static func getGeneralMenu() -> [GeneralMenuForTheApplication] {
-    
+    static func getGeneralMenu() -> [GeneralMenuForTheApplication] {
+        let information = InfoDishesDataManager.sharedDishes
+        
         return [
-            GeneralMenuForTheApplication(name: "Рыба с рисом",
-                                         mainPosition: .fish,
-                                         ingredients: ["Рыба", "Томат",
-                                                       "Лук", "Лимон"],
-                                         garnish: "Рис"),
+            GeneralMenuForTheApplication(
+                name: "Рыба с рисом",
+                mainPosition: .fish,
+                ingredients: ["Рыба", "Томат",
+                              "Лук", "Лимон"],
+                garnish: "Рис",
+                information: information.fishWithRiceAndVegetablesInfo
+            ),
             
-            GeneralMenuForTheApplication(name: "Кортофель по деревенски",
-                                         mainPosition: .fastFood,
-                                         ingredients: ["Соль", "Чеснок",
-                                                       "Картофель"]),
+            GeneralMenuForTheApplication(
+                name: "Кортофель по деревенски",
+                mainPosition: .fastFood,
+                ingredients: ["Соль", "Чеснок",
+                              "Картофель"],
+                information: information.rusticPotatoInfo
+            ),
             
-            GeneralMenuForTheApplication(name: "Борщ",
-                                         mainPosition: .soup,
-                                         ingredients: ["Картофель", "Свекла",
-                                                       "Морковь", "Лук",
-                                                       "Капуста"]),
+            GeneralMenuForTheApplication(
+                name: "Борщ",
+                mainPosition: .soup,
+                ingredients: ["Картофель", "Свекла",
+                              "Морковь", "Лук",
+                              "Капуста"],
+                information: information.borschtInfo
+            ),
             
-            GeneralMenuForTheApplication(name: "Шашлык из свинины",
-                                         mainPosition: .meet,
-                                         ingredients: ["Свинина", "Лук", "Зелень"],
-                                         garnish: "Картофель отварной"),
+            GeneralMenuForTheApplication(
+                name: "Шашлык из свинины",
+                mainPosition: .meet,
+                ingredients: ["Свинина", "Лук", "Зелень"],
+                garnish: "Картофель отварной",
+                information: information.porkSkewers
+            ),
             
-            GeneralMenuForTheApplication(name: "Салат цезарь для Тёмы",
-                                         mainPosition: .vegetables,
-                                         ingredients: ["Зеленый салат", "Помидоры",
-                                                       "Куриное филе","Соус 'Цезарь'",
-                                                       "Сливочное масло", "Чеснок",
-                                                       "Сыр пармезан"])
+            GeneralMenuForTheApplication(
+                name: "Салат цезарь для Тёмы",
+                mainPosition: .vegetables,
+                ingredients: ["Зеленый салат", "Помидоры",
+                              "Куриное филе","Соус 'Цезарь'",
+                              "Сливочное масло", "Чеснок",
+                              "Сыр пармезан"],
+                information: information.caesarSalad
+            )
         ]
     }
 }
